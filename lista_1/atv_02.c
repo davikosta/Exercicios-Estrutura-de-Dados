ee2eb26d04a5df56c9374cff428507d2f618228a
Davi Costa <contato.davikosta@gmail.com>
2026-06-12 11:36:31 -0300

    Implementa solução da atividade 2, adiciona exe's ao gitignore, refatora gitignore pra .gitignore

--- lista_1/atv_02.c
+++ lista_1/atv_02.c
@@ -3,78 +3,119 @@
 
 #define MAX 100
 
-typedef struct {
+typedef struct
+{
     int chave;
 } tipoItem;
 
-typedef struct {
+typedef struct
+{
     tipoItem item[MAX];
     int tamanho;
 } tipoLista;
 
-int obtemN() {
+int obtemN()
+{
     int n;
     scanf("%d", &n);
     return n;
-    
 }
 
-int obtemP() {
+int obtemP()
+{
     int p;
     scanf("%d", &p);
     return p;
 }
 
-int obtemX() {
+int obtemX()
+{
     int x;
     scanf("%d", &x);
     return x;
 }
 
-void inicializaLista(tipoLista *lista, int n) {
+void inicializaLista(tipoLista *lista, int n)
+{
     lista->tamanho = n;
 
     for (int i = 0; i < lista->tamanho; i++)
     {
         scanf("%d", &lista->item[i].chave);
     }
-    
 }
 
-void insereLista(tipoLista *lista, int p, int x) {
+void insereLista(tipoLista *lista, int p, int x)
+{
 
-    if(lista->tamanho < 100 && p <= lista->tamanho + 1 && p > 0) {
-        for(int i = lista->tamanho; i >= p; i--) {
-            lista->item[i].chave = lista->item[i-1].chave;
+    if (lista->tamanho < 100 && p <= lista->tamanho + 1 && p > 0)
+    {
+        for (int i = lista->tamanho; i >= p; i--)
+        {
+            lista->item[i].chave = lista->item[i - 1].chave;
         }
-        lista->item[p-1].chave = x;
+        lista->item[p - 1].chave = x;
         lista->tamanho++;
 
-        for(int i = 0; i < lista->tamanho; i++) {
+        for (int i = 0; i < lista->tamanho; i++)
+        {
             printf("%d\n", lista->item[i].chave);
         }
-    } else {
+    }
+    else
+    {
         printf("Lista esta cheia ou posicao inválida\n");
     }
 }
 
-void verificaOrdenado (tipoLista *lista) {
-        if (lista->tamanho > 0) {
-            for (int i = 0; i < lista->tamanho-1; i++) {
-                if(lista->item[i].chave > lista->item[i+1].chave) {
+int verificaOrdenado(tipoLista *lista) {
 
-                }
+    int crescente = 0, decrescente = 0;
+    if (lista->tamanho > 0) {
+        for (int i = 0; i < lista->tamanho - 1; i++){
+            if (lista->item[i].chave < lista->item[i+1].chave) {
+                crescente = 1;
+            } else {
+                decrescente = 2;
             }
         }
+        if (crescente == 1 && decrescente == 0) {
+            return crescente;
+        } else if (crescente == 0 && decrescente == 2) {
+            return decrescente;
+        } else {
+            return -1;
+        }
+    }
+
     return 0;
 }
 
-int main() {
+void exibeOdernacao(tipoLista *lista) {
+    switch (verificaOrdenado(lista)) {
+        case 0:
+            printf("LISTA VAZIA");
+            break;
+        case 1:
+            printf("CRESCENTE");
+            break;
+        case 2:
+            printf("DECRESCENTE");
+            break;
+        case -1:
+            printf("DESORDENADA");
+    }
+}
+
+int main()
+{
     setlocale(LC_ALL, "Portuguese");
-    
+
     tipoLista lista;
 
     inicializaLista(&lista, obtemN());
+
+    exibeOdernacao(&lista);
 
     return 0;
 }
